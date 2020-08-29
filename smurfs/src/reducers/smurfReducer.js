2d1f1c816ecfd564796d68abc78cd80e164f3dc3
Liz Shulman <corgipower@hotmail.com>
2020-08-29 12:22:51 -0400

    reducer, action files created, indexjs done

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,13 @@
+export const initialState = {
+}
+
+export const START_GETTING_SMURF = "START_GETTING_SMURF";
+export const SUCCESS_GETTING_SMURF = "SUCCESS_GETTING_SMURF";
+export const ERROR_GETTING_SMURF = "ERROR_GETTING_SMURF";
+
+export const smurfReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
